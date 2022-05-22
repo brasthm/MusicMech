4b03964304fbac79ac9ddf288ce922749fa1121c
Cyril <cyril.li@univ-st-etienne.fr>
2022-05-22 17:18:52 +0200

    hitsound

--- src/Mechanic.cpp
+++ src/Mechanic.cpp
@@ -3,8 +3,20 @@
 //
 
 #include "Mechanic.h"
-
+#include "RessourceLoader.h"
 
 Mechanic::Mechanic() {
     beat_ = 0;
+    played_ = false;
+}
+
+void Mechanic::playSound() {
+    if(!played_) {
+        sound_.play();
+        played_ = true;
+    }
+}
+
+void Mechanic::setSoundName(const std::string& name) {
+    sound_.setBuffer(RessourceLoader::getSoundBuffer(name));
 }
